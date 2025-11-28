335bf3299172928937df97898d6cd2d07d98ef3d
Krishna B <chundakrishna@gmail.com>
2025-11-29 00:24:24 +0530

    incorporating pr feeedback

--- src/type-clipboard.ts
+++ src/type-clipboard.ts
@@ -4,9 +4,6 @@ import { promisify } from "node:util";
 
 const execFileAsync = promisify(execFile);
 
-interface Preferences {
-  delay: string;
-}
 
 export default async function Command() {
   const latestClipboardItem = await Clipboard.readText();
